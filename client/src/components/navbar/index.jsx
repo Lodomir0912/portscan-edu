92b14e910ad60e0e73c50df68fa6abbd2a9c3e6c
cwitkows <01187393@pw.edu.pl>
2025-05-15 13:37:40 +0200

    navbar

--- client/src/components/navbar/index.jsx
+++ client/src/components/navbar/index.jsx
@@ -3,15 +3,33 @@ import '../../App.css';
 
 const NavBar = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">PortScan-Edu</div>
-      <ul className="nav-links">
-        <li><a href="/">Main Page</a></li>
-        <li><a href="/about">Tools</a></li>
-        <li><a href="/contact">Kontakt</a></li>
-      </ul>
-    </nav>
+    <div className="container">
+      <nav className="navbar">
+        <div className="logo">
+          <a href="/" className="logo-link">PortScan-Edu</a>
+        </div>
+        <ul className="nav-links">
+          <li><a href="/">Main Page</a></li>
+          <li><a href="/scan">Scan</a></li>
+          <li><a href="/login">Login</a></li>
+          <li>
+            <div class="dropdown">
+              <button class="dropbtn">Tools
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                <a href="/nmap">nmap</a>
+                <a href="/snort">snort</a>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <main className="main-content">
+        <h1>Welcome to PortScan-Edu!</h1>
+      </main>
+    </div>
   );
-};
+}
 
 export default NavBar;
